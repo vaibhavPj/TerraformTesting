096cb990bea61dc4f82c28bb5a41190bb250153d
vaibhavPj <101042816+vaibhavPj@users.noreply.github.com>
2022-03-20 13:36:42 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,18 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.34.0"
+    }
+  }
+}
 
+provider "azurerm" {
+    version = "2.34.0"
+    subscription_id = var.subscription_id
+    client_id       = var.client_id
+    client_secret   = var.client_secret
+    tenant_id       = var.tenant_id
+  features {}
+}
