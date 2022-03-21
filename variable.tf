c1c92c3d9794a99c592970acf8a1635a231c504c
vaibhavPj <101042816+vaibhavPj@users.noreply.github.com>
2022-03-21 14:02:34 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,16 +1,20 @@
 variable "subscription_id" {
     description = "The subscription ID to be used to connect to Azure"
     type = string
+    default = "8d02e2fa-890c-41af-9b2f-e60a65ae691b"
 }
 variable "client_id" {
     description = "The client ID to be used to connect to Azure"
     type = string
+    default = "3f67af80-ca15-4032-a37b-1f9f212aab4f"
 }
 variable "client_secret" {
     description = "The client secret to be used to connect to Azure"
     type = string
+    default = "GAg7Q~ilP-t5jbLiOPA5XybDJxXrdb5nlbPfA"
 }
 variable "tenant_id" {
     description = "The tenant ID to be used to connect to Azure"
     type = string
+    default = "679fabea-0729-4939-ae5a-2501cd2a1f7d"
 }
