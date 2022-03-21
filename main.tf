82076f0672cd9dbcafe95379d67ad2e64b3bb56c
vaibhavPj <101042816+vaibhavPj@users.noreply.github.com>
2022-03-21 15:40:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,42 +1,26 @@
-provider "azurerm" {
-  version = "=2.5.0"
-  features {}
+resource "azurerm_resource_group" "Rg" {
+  name     = "Rg-resources"
+  location = "West Europe"
 }
 
-## <https://www.terraform.io/docs/providers/azurerm/r/resource_group.html>
-resource "azurerm_resource_group" "rg" {
-  name     = "TerraformTesting"
-  location = "eastus"
-}
-
-## <https://www.terraform.io/docs/providers/azurerm/r/availability_set.html>
-resource "azurerm_availability_set" "DemoAset" {
-  name                = "example-aset"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-## <https://www.terraform.io/docs/providers/azurerm/r/virtual_network.html>
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vNet"
+resource "azurerm_virtual_network" "Vnet" {
+  name                = "Vnet-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.Rg.location
+  resource_group_name = azurerm_resource_group.Rg.name
 }
 
-## <https://www.terraform.io/docs/providers/azurerm/r/subnet.html> 
 resource "azurerm_subnet" "subnet" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix       = "10.0.2.0/24"
+  resource_group_name  = azurerm_resource_group.Rg.name
+  virtual_network_name = azurerm_virtual_network.Vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
-## <https://www.terraform.io/docs/providers/azurerm/r/network_interface.html>
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_network_interface" "nic" {
+  name                = "Rg-nic"
+  location            = azurerm_resource_group.Rg.location
+  resource_group_name = azurerm_resource_group.Rg.name
 
   ip_configuration {
     name                          = "internal"
@@ -45,17 +29,15 @@ resource "azurerm_network_interface" "example" {
   }
 }
 
-## <https://www.terraform.io/docs/providers/azurerm/r/windows_virtual_machine.html>
-resource "azurerm_windows_virtual_machine" "example" {
-  name                = "example-machine"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+resource "azurerm_windows_virtual_machine" "Windows_machine" {
+  name                = "Windows-machine"
+  resource_group_name = azurerm_resource_group.Rg.name
+  location            = azurerm_resource_group.Rg.location
   size                = "Standard_F2"
   admin_username      = "adminuser"
   admin_password      = "P@$$w0rd1234!"
-  availability_set_id = azurerm_availability_set.DemoAset.id
   network_interface_ids = [
-    azurerm_network_interface.example.id,
+    azurerm_network_interface.nic.id,
   ]
 
   os_disk {
