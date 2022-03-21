ed5718de967aa15bf9fffa060b4fdd6e40c26eec
vaibhavPj <101042816+vaibhavPj@users.noreply.github.com>
2022-03-21 14:10:40 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,13 +3,13 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "2.34.0"
+      version = "=2.97.0"
     }
   }
 }
 
 provider "azurerm" {
-    version = "2.34.0"
+    version = "=2.97.0"
     subscription_id = var.subscription_id
     client_id       = var.client_id
     client_secret   = var.client_secret
